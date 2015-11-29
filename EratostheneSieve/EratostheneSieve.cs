054d86d2e77d3feb63662a49751fc161d5a6c1cc
simone.deponti <simone.deponti@gmail.com>
2015-11-29 16:27:37 +0000

    Initial commit

---
+++ EratostheneSieve/EratostheneSieve.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace EratostheneSieve
+{
+    public class Sieve
+    {
+        public static int[] Compute(int ceil)
+        {
+            int i, j;
+            bool[] A;
+            int[] results;
+
+            if(ceil < 2)
+            {
+                results = new int[0];
+            }
+            else
+            {
+                A = (from n in Enumerable.Range(0, ceil) select true).ToArray();
+                for (i = 2; i < Math.Sqrt((double) ceil); i++)
+                {
+                    if(A[i])
+                    {
+                        for(j = (i*i); j < ceil; j+=i)
+                        {
+                            A[j] = false;
+                        }
+                    }
+                }
+                results = (from n in Enumerable.Range(1, ceil - 1) where A[n] select n).ToArray();
+            }
+
+            return results;
+        }
+    }
+}
---
+++ EratostheneSieve/client.py
@@ -0,0 +1,14 @@
+﻿import clr
+import os
+clr.AddReferenceToFileAndPath(
+    os.path.join(os.path.dirname(__file__), r"bin\Debug\EratostheneSieve.dll")
+)
+import EratostheneSieve
+
+
+if __name__ == '__main__':
+    for ceil in (0, 1, 2, 3, 7, 13, 28):
+        print("Computing sieve for {}: {}".format(
+            ceil,
+            [i for i in EratostheneSieve.Sieve.Compute(ceil)]
+        ))
